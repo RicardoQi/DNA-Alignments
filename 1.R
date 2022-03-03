b4b5a1bc0ec3022ab0aa6788a6c1bc79d8d265aa
RicardoQi <qi19981010@sina.com>
2022-03-03 12:55:18 +0800

    rstudio

--- 1.R
+++ 1.R
@@ -1,8 +1,10 @@
+#Github link https://github.com/RicardoQi/DNA-Alignments
+#load packages
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("genbankr")
-install("Biostrings)
+install("Biostrings")
 install("ggtree")
 install("annotate", force= TRUE)
 install("muscle")
@@ -14,16 +16,17 @@ library(rentrez)
 library(genbankr)
 library(annotate)
 library(ape)
-
-BbID<-GBAccession("OM797449.1")
+#enter the ID of the gene and load it to the program
+BbID<-GBAccession("NC_045512.2")
 BbGbk<-readGenBank(BbID)
+#indentify the classes
 class(BbGbk)
+#use str() to see the structure
 str(BbGbk)
+#coding sequence of the gene
 cds(BbGbk)
+#trasncripts of the gene
 transcripts(BbGbk)
 exons(BbGbk)
 genes(BbGbk)
 otherFeatures(BbGbk)
-
-
-BbGbk@translation
